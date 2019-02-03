bf63e2d47ee4a5cca155034d96edf5b1997ae2a8
Lei Xian <35618990+floraleilei@users.noreply.github.com>
2019-02-02 19:42:50 -0500

    get can not return myftp and get cannot work after put. still working on.

--- DSP1/Client/myftp.java
+++ DSP1/Client/myftp.java
@@ -38,38 +38,47 @@ public class myftp {
 	// --------------------- GET FILE FROM SERVER - RECEIVE FROM SERVER ------------------------
 	public static void getCommand(String fileDirName, DataInputStream dis) throws IOException {
 
-		//get file size
-		String fileSize = dis.readLine();
+		String chunks = dis.readLine();
 		FileOutputStream f = new FileOutputStream(new File(fileDirName));
 		int count = 0;
-		byte[] buffer = new byte[8192];
-		long bytesReceived = 0;
-		while(bytesReceived < Integer.parseInt(fileSize)) {
+		byte[] buffer = new byte[8 * 1024];
+		int chunk_num = 0;
+		while(chunk_num < Integer.parseInt(chunks)) {
 			count = dis.read(buffer);
 			f.write(buffer, 0, count);
-			bytesReceived += count;
+			chunk_num += 1;	
 		};
+	
 		f.close();
+		System.out.println("myftp> "+ "\n");
 	}
 
 	// ------------------- PUT FILE TO SERVER - SEND TO SERVER--------------------
 	public static void putCommand(String fileDirName,DataInputStream dis) throws IOException {
 		File file = new File(fileDirName);
+		
 		if(file.exists()) {
 			//get file size
 			long fileSize = file.length();
+			double sizeOfFiles = 8.0 * 1024;
+			int chunks = (int) Math.ceil(fileSize/sizeOfFiles);
 			//send file size
-			dos.writeBytes(fileSize + "\n");
-			byte[] buffer = new byte[8192];
-			BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-			int count = 0;
-			while((count = in.read(buffer)) > 0)
-			dos.write(buffer, 0, count);
-			in.close();
+			dos.writeBytes(fileSize + chunks + "\n");
+			//dos.writeBytes(chunks + "\n");
+			byte[] buffer = new byte[(int) sizeOfFiles];
+			try (FileInputStream fis = new FileInputStream(file);
+					BufferedInputStream bis = new BufferedInputStream(fis)){
+					int count = 0;
+					while((count = bis.read(buffer)) > 0) {
+		                	dos.write(buffer, 0, count);
+					}
+			
 		}
-			else{
+			
+		}
+			else {
 				System.out.println("transfer error: " + fileDirName);
-			}
+			}	
 
 	}
 	
